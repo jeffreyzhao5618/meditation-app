9145baaaa895620ec7852d1cb04e31683c9b0ce0
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2022-02-07 18:51:03 -0600

    Added numbers to number select

--- app/src/main/java/com/cdtgrss/meditationapp/TimerSettingsFragment.kt
+++ app/src/main/java/com/cdtgrss/meditationapp/TimerSettingsFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.NumberPicker
+import androidx.databinding.DataBindingUtil
+import com.cdtgrss.meditationapp.databinding.FragmentTimerSettingsBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +15,37 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TimerSettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentTimerSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_timer_settings, container, false)
+
+        // Setup number pickers
+        setupNumPicker(binding.hourNumberPicker)
+        setupNumPicker(binding.minuteNumberPicker)
+        setupNumPicker(binding.secondNumberPicker)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer_settings, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TimerSettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TimerSettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    /**
+     * This function is used to setup the three number pickers used to change the timer's value.
+     *
+     * @param numberPicker The NumberPicker to setup
+     */
+    private fun setupNumPicker (numberPicker: NumberPicker) {
+        numberPicker.apply {
+            minValue = 0
+            maxValue = 59
+            displayedValues =
+                resources.getStringArray(R.array.timer_settings_num_picker_string_array)
+        }
     }
+
 }
