9342d32f2aa418f0f06edba7d167776ef34bd923
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2022-02-07 19:48:04 -0600

    added navigate up to action bar

--- app/src/main/java/com/cdtgrss/meditationapp/MainActivity.kt
+++ app/src/main/java/com/cdtgrss/meditationapp/MainActivity.kt
@@ -3,13 +3,26 @@ package com.cdtgrss.meditationapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.cdtgrss.meditationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // Enable up button
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
